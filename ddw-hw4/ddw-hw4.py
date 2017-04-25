dab479243c285838d0afcb0f7c27c641893a40ae
Petr Suchý <peta.suchy@gmail.com>
2017-04-25 02:26:19 +0200

    hw04: completed. With results.

--- ddw-hw4/ddw-hw4.py
+++ ddw-hw4/ddw-hw4.py
@@ -4,7 +4,7 @@ import networkx as nx
 import pprint as pp
 
 
-def loadInput(inputFile):
+def loadInput(inputFile, limit):
     text = None
     with open(inputFile, 'r') as f:
         text = f.read()
@@ -13,7 +13,11 @@ def loadInput(inputFile):
     casts = dict()
 
     lines = text.splitlines()
+    linescount=0
     for line in lines:
+        linescount+=1
+        if linescount > limit:
+             break
         movie = line.split(";")[1].split("\"")[1]
         actor = line.split(";")[2].split("\"")[1]
         if len(actor) > 5:
@@ -33,7 +37,7 @@ def loadInput(inputFile):
     return graph
 
 
-def printBasic(graph):
+def printBasics(graph):
     print("Graph has {} nodes, {} edges and {} components.".format(graph.number_of_nodes(), graph.number_of_edges(),nx.number_connected_components(graph)))
     print("It's density is {}".format(nx.density(graph)))
 
@@ -46,8 +50,8 @@ def printCentralities(graph, full):
         graph.node[actor]["dg_cent"] = centrality
     for actor, centrality in eigenvectorCentrality.items():
         graph.node[actor]["ev_cent"] = centrality
-    print("Top5 by degree centrality: {}".format(sorted(degreeCentrality.items(), key=operator.itemgetter(1), reverse=True)[:5]))
-    print("Top5 by eigen vector centrality: {}".format(sorted(eigenvectorCentrality.items(), key=operator.itemgetter(1), reverse=True)[:5]))
+    print("\tTop5 by degree centrality: {}".format(sorted(degreeCentrality.items(), key=operator.itemgetter(1), reverse=True)[:5]))
+    print("\tTop5 by eigen vector centrality: {}".format(sorted(eigenvectorCentrality.items(), key=operator.itemgetter(1), reverse=True)[:5]))
 
     if full:
         closenessCentrality = nx.closeness_centrality(graph)
@@ -59,6 +63,30 @@ def printCentralities(graph, full):
         print("Top5 by closeness centrality: {}".format(sorted(closenessCentrality.items(), key=operator.itemgetter(1), reverse=True)[:5]))
         print("Top5 by betweenness centrality: {}".format(sorted(betweennessCentrality.items(), key=operator.itemgetter(1), reverse=True)[:5]))
 
+def printCommunities(graph):
+    communities = {}
+    for community_id, community in enumerate(nx.k_clique_communities(graph, 3)):
+        for node in community:
+            communities[node] = community_id + 1
+
+    community_actors = {}
+    for key, val in communities.items():
+        if val not in community_actors:
+            community_actors[val] = []
+        community_actors[val].append(key)
+
+    print("\nCommunities:")
+    print("\t5 biggest communities size: ", end="")
+    for comm in sorted(community_actors.items(), key= lambda item: len(item[1]), reverse=True)[:5]:
+        print(len(comm[1]), end=",")
+    print("\n\tMembers of biggest community:")
+    for comm in sorted(community_actors.items(), key=lambda item: len(item[1]), reverse=True)[:1]:
+        print("\t{}".format(comm[1]))
+
+    for actor, community_id in communities.items():
+        graph.node[actor]['community_id'] = community_id
+
+
 def printKevinBacon(graph):
     print("\nKevin Bacon Number to James Stewart:")
     kbnumbers = nx.single_source_shortest_path_length(graph, "James Stewart")
@@ -68,18 +96,19 @@ def printKevinBacon(graph):
         graph.node[actor]["KevinBaconNumber"]=kbnumber
         sum += kbnumber
         actors +=1
-    print("Average Kevin Becon number is {}".format(sum/actors))
-    print("Lowest (closest) top10: {}".format(sorted(kbnumbers.items(), key=operator.itemgetter(1), reverse=False)[:10]))
-    print("Highest (farthest) top10: {}".format(sorted(kbnumbers.items(), key=operator.itemgetter(1), reverse=True)[:10]))
+    print("\tAverage Kevin Becon number is {}".format(sum/actors))
+    print("\tLowest (closest) top10: {}".format(sorted(kbnumbers.items(), key=operator.itemgetter(1), reverse=False)[:10]))
+    print("\tHighest (farthest) top10: {}".format(sorted(kbnumbers.items(), key=operator.itemgetter(1), reverse=True)[:10]))
 
 def exportGraph(graph):
     nx.write_gexf(graph, 'graph.gexf')
 
 # MAIN
 inputFile = "/home/petr/skola/DDW/ddw-hw4/casts.csv"
-graph = loadInput(inputFile)
-# printBasic(graph)
-# printCentralities(graph, False)
+graph = loadInput(inputFile, 20000)
+printBasics(graph)
+printCentralities(graph, True)
+printCommunities(graph)
 printKevinBacon(graph)
 exportGraph(graph)
 
